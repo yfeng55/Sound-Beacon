191e17b17085f904f66da0919e6e4cf16379a6ff
Yijie <yijiefeng@gmail.com>
2014-06-26 14:28:30 -0700

    authtoken sharedprferences

--- src/com/example/songbeacon/MainActivity.java
+++ src/com/example/songbeacon/MainActivity.java
@@ -1,31 +1,51 @@
 package com.example.songbeacon;
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
-import com.firebase.client.FirebaseError;
-import com.firebase.client.ValueEventListener;
 
 
 public class MainActivity extends Activity{
 	
 	EditText namefield;
 	Button registerbutton;
+	SharedPreferences prefs;
 	
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
 		
-		namefield = (EditText) findViewById(R.id.et_name);
-		registerbutton = (Button) findViewById(R.id.b_register);
+		//obtain sharedpreferences
+		prefs = this.getSharedPreferences("com.example.songbeacon", Context.MODE_PRIVATE);
 		
-/*		Firebase ref = new Firebase("https://resplendent-fire-3957.firebaseio.com/");
+		if(prefs.getBoolean("authtoken", false) == false){
+			
+			Log.i("EEE", "not logged in - the authtoken is false");
+			
+			setContentView(R.layout.activity_main);
+			
+			namefield = (EditText) findViewById(R.id.et_name);
+			registerbutton = (Button) findViewById(R.id.b_register);
+			
+			
+			
+			
+			
+		}else{
+			Log.i("EEE", "logged in - the authtoken is true");
+			
+		}
+		
+		
+		/*Firebase ref = new Firebase("https://resplendent-fire-3957.firebaseio.com/");
 		ref.addValueEventListener(new ValueEventListener() {
 			
 			@Override
@@ -42,13 +62,56 @@ public class MainActivity extends Activity{
 		//get a reference to the FireBase root
 		Firebase ref = new Firebase("https://resplendent-fire-3957.firebaseio.com/");
 		
-		
 		String name;
 		name = namefield.getText().toString();
 		
 		Firebase newpushref = ref.push();
 		newpushref.setValue(name);
+		
+		//create a new editor for the prefs object
+		Editor editor = prefs.edit();
+				
+		//store the authtoken as "true"
+		editor.putBoolean("authtoken", true);
+		editor.commit();
+		
+		//display logged-in layout
+		
 	}
 	
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
