bee31c4cc0280ab0a4f05bfb0e6dc85fd7cb1280
Yijie <yijiefeng@gmail.com>
2014-06-26 23:45:29 -0700

    asdf

--- src/com/example/songbeacon/EditUser.java
+++ src/com/example/songbeacon/EditUser.java
@@ -2,9 +2,12 @@ package com.example.songbeacon;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.firebase.client.Firebase;
 
 
 public class EditUser extends Activity{
@@ -29,6 +32,16 @@ public class EditUser extends Activity{
 		//set form fields to the user's attribute values
 		namefield.setText(u.getName());
 		songfield.setText(u.getSongID());
+	}
+	
+	
+	
+	
+	public void writeToFirebase(View v){
+		
+		Firebase ref = new Firebase("https://resplendent-fire-3957.firebaseio.com/");
+		
+		Firebase userref = ref.child("");
 	}
 	
 	
